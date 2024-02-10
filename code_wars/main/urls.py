fe462fad0a53ca1673ef51397626429161d5d190
Rajasimhareddybolla <rajasimhabolla@gmail.com>
2024-02-11 00:31:22 +0530

    completed all pages backend need to implementation

--- code_wars/main/urls.py
+++ code_wars/main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("prb",views.prb,name="prb"),
+    path("frnds",views.frnds,name="friends"),
+    path("code",views.codespace,name="code")
 ]
--- code_wars/main/views.py
+++ code_wars/main/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"main/index.html")
+def prb(request):
+    return render(request,"main/problem.html")
+def frnds(request):
+    return render(request,"main/friends_con.html")
+def codespace(request):
+    return render(request,"main/codespace.html")
